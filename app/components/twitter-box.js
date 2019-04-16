50ac612fce4d9caf882c9308f514262f0d23fdaf
ctellezesp <a01274912@itesm.mx>
2019-04-15 20:02:46 -0500

    post added

--- app/components/twitter-box.js
+++ app/components/twitter-box.js
@@ -1,21 +1,25 @@
 import Component from '@glimmer/component';
-import {tracked} from '@glimmer/tracking'; //glimer es el motor de ember para componentes
-import {action} from '@ember/object'; //importar libreria de acciones
+import { tracked } from '@glimmer/tracking'; //glimer es el motor de ember para componentes
+import { action } from '@ember/object'; //importar libreria de acciones
 
 export default class TwitterBoxComponent extends Component {
 	@tracked text;
 	@tracked error;
+	@tracked img = "https://avatars0.githubusercontent.com/u/26472750?s=460&v=4";
+	@tracked user = 'ctellezesp';
 
 	@action handleChange(text){
 		this.text = text;
-		//this.error = error;
+		if(this.error)
+			this.error = error;
 	}
 
 	@action 
 	async handleSubmit(){
 		let text = this.text;
-		let req = await this.args.onSubmit(text);
-		this.error = req.error;
+		let userName = this.user;
+		let avatar = this.img;
+		await this.args.handleSubmit(text, userName, avatar);
 		this.text = '';
 	}
 
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,17 +1,16 @@
 import Controller from '@ember/controller';
-import {tracked} from '@glimmer/tracking';
-import{action} from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
 
 export default class ApplicationController extends Controller {
 	@action 
-	onSubmit(text){
-		let tweetsCopy = this.tweets.slice();
-
-		this.tweets = tweetsCopy.concat({
-			id: Math.floor(Math.random() * 100) + 3,
-			user_name: 'Carlos',
-			description: text,
-			created_at: '2019-04-11'
+	onSubmit(text, userName, avatar){
+		let post = this.store.createRecord('tweet', {
+		  description: text,
+		  avatar: avatar,
+		  userName: userName
 		});
+		post.save();
 	}
 }
